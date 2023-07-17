f9798ed7899bd2f5548534c5f2cb34f4848040e3
maxwynne <76392789+maxwynne@users.noreply.github.com>
2023-07-17 20:49:24 +0100

    Update struct_1.cpp

--- struct_1.cpp
+++ struct_1.cpp
@@ -7,13 +7,21 @@ struct myGames {
 } Game1, Game2;
 
 // first structure
-Game1.brand = "Half Life";
-Game1.year = 1998;
+int main() {
+  // declared variable myGames that uses the myGames structure, structure is now data type
+  // of the variable
+  myGames Game1;
+  Game1.brand = "Half Life";
+  Game1.year = 1998;
 
-// second structure
-Game2.brand = "Half Life 2";
-Game2.year = 2004;
+  // second structure
+  myGames Game2;
+  Game2.brand = "Half Life 2";
+  Game2.year = 2004;
 
-// print structure members
-std::cout << Game1.brand << " " << Game1.year << " " << "\n";
-std::cout << Game2.brand << " " << Game2.year << " " << "\n";
+  // print structure members
+  std::cout << Game1.brand << " " << Game1.year << " " << "\n";
+  std::cout << Game2.brand << " " << Game2.year << " " << "\n";
+
+  return 0;
+}
