9439a44e03f3887940a6781e7908136d0c4fe25b
maxwynne <76392789+maxwynne@users.noreply.github.com>
2023-07-17 20:44:31 +0100

    Create struct_1.cpp

---
+++ struct_1.cpp
@@ -0,0 +1,19 @@
+// use one structure to represent two games
+
+struct {
+  string brand; 
+  string model;
+  int year;
+} Game1, Game2;
+
+// first structure
+Game1.brand = "Half Life";
+Game1.year = 1998;
+
+// second structure
+Game2.brand = "Half Life 2";
+Game2.year = 2004;
+
+// print structure members
+std::cout << Game1.brand << " " << Game1.year << " " << "\n";
+std::cout << Game2.brand << " " << Game2.year << " " << "\n";
