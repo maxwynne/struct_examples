d8f7fd1568602897f7f946a648dc718f7042a3f2
maxwynne <76392789+maxwynne@users.noreply.github.com>
2023-07-17 22:20:49 +0100

    Create simple_vertex_structure.cpp

---
+++ simple_vertex_structure.cpp
@@ -0,0 +1,13 @@
+// use C++ program to define simple vertex structure - generate 5 random vertices and write them to terminal
+
+#include <iostream>
+#include <cstdlib> 
+
+using namespace std;
+
+int main()
+{
+  struct Vertex {
+    int x;
+    int y;
+  };
